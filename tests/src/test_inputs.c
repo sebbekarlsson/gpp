e2feed394f083369869023e8e48fd0fea3bb1bc1
sebastian karlsson <sebbekarlsson97@gmail.com>
2023-05-05 01:27:59 +0200

    fix broken tests

--- tests/src/test_inputs.c
+++ tests/src/test_inputs.c
@@ -3,14 +3,21 @@
 #include "include/utils.h"
 #include <string.h>
 
+
+//gpp_result_T *gpp_eval(char *source, unsigned int lazy, AST_T *parent,
+//                     AST_T *context, GPPEnv* env, GPPEnv* global_env);
+
 void test_inputs(char* argv[]) {
 
+  
+  const char* filepath = "inputs/tmp.html";
   GPPEnv env = (GPPEnv){ .base_path = "inputs" };
+  GPPEnv global_env = (GPPEnv){ .base_path = filepath };
 
   {
     printf("Running test: hello-world.html\n");
     gpp_result_T *res =
-      gpp_eval(gpp_read_file("inputs/hello-world.html"), 0, 0, 0, &env);
+      gpp_eval(gpp_read_file("inputs/hello-world.html"), 0, 0, 0, &env, &global_env);
 
 
     test_assert(!!res->res, "res->res exists");
@@ -21,7 +28,7 @@ void test_inputs(char* argv[]) {
   {
     printf("Running test: arrow-function.html\n");
     gpp_result_T *res =
-        gpp_eval(gpp_read_file("inputs/arrow-function.html"), 0, 0, 0, &env);
+      gpp_eval(gpp_read_file("inputs/arrow-function.html"), 0, 0, 0, &env, &global_env);
     test_assert(!!res->res, "res->res exists");
     test_assert(strcmp(res->res, "<li>hello</li><li>world</li>") == 0,
                 "Receives correct output");
@@ -30,7 +37,7 @@ void test_inputs(char* argv[]) {
   {
     printf("Running test: map-context.html\n");
     gpp_result_T *res =
-        gpp_eval(gpp_read_file("inputs/map-context.html"), 0, 0, 0, &env);
+      gpp_eval(gpp_read_file("inputs/map-context.html"), 0, 0, 0, &env, &global_env);
     test_assert(!!res->res, "res->res exists");
     test_assert(strcmp(res->res, "<li>Not</li>\n"
                                  "<li>Made</li>\n"
@@ -46,7 +53,7 @@ void test_inputs(char* argv[]) {
 
   {
     printf("Running test: escape.html\n");
-    gpp_result_T *res = gpp_eval(gpp_read_file("inputs/escape.html"), 0, 0, 0, &env);
+    gpp_result_T *res = gpp_eval(gpp_read_file("inputs/escape.html"), 0, 0, 0, &env, &global_env);
     test_assert(!!res->res, "res->res exists");
     test_assert(strcmp(res->res, "&lt;h1&gt;Hello&lt;/h1&gt;") == 0,
                 "Receives correct output");
